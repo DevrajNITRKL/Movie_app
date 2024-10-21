f696c8ad3665a7638d16e284979861d2d8020c80
DevrajNITRKL <devrajsingh86313@gmail.com>
2024-10-21 20:08:10 +0530

    Upto Subscribe to store

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 class Navbar extends React.Component {
   render(){
     return (
-        <div className="nav">
+        <div className="navbar">
           <div className="search-container">
             <input />
             <button id="search-btn">Search</button>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
+import movies from './reducers'
+
+const store = createStore(movies);
+console.log('store',store);
+
+// console.log('Before State',store.getState());
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies : [{name : 'Superman'}]
+// });
+// console.log('After State',store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <App store={store}/>
+  
 );
 
