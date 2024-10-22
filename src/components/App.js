e2eef51b4efd8106b38115c1757faaf6db78e2f2
DevrajNITRKL <devrajsingh86313@gmail.com>
2024-10-22 23:16:36 +0530

    changed the State

--- src/components/App.js
+++ src/components/App.js
@@ -19,8 +19,9 @@ class App extends React.Component {
   }
   
   render(){
-    const movies = this.props.store.getState();
-    console.log('RENDERED')
+    // const movies = this.props.store.getState();
+    const { list} = this.props.store.getState();
+    console.log('RENDERED', this.props.store.getState())
     return (
       <div className="App">
         <Navbar />
@@ -30,7 +31,7 @@ class App extends React.Component {
             <div className='tab'>Favourites</div>
           </div>
           <div className='list'>
-            {movies.map((movie, index) =>{
+            {list.map((movie, index) =>{
               <MovieCard movie={movie} key={`movies-${index}`}/>
             })}
           </div>
